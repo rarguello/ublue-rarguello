ef4ad1a59d8fb100418a48d5189152e419792306
Ricardo Arguello <ricardo.arguello@gmail.com>
2023-06-29 02:23:01 -0500

    Install Flatpaks in system

--- scripts/post/autorun.sh
+++ scripts/post/autorun.sh
@@ -6,16 +6,16 @@ echo "Add Flathub"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 echo "Install Flatpaks"
-flatpak install flathub com.google.Chrome
-flatpak install flathub com.raggesilver.BlackBox
-flatpak install flathub com.slack.Slack
-flatpak install flathub com.spotify.Client
-flatpak install flathub org.gnome.meld
-flatpak install flathub org.libreoffice.LibreOffice
-flatpak install flathub org.mozilla.firefox
-flatpak install flathub org.mozilla.Thunderbird
-flatpak install flathub org.telegram.desktop
-flatpak install flathub us.zoom.Zoom
+flatpak install --system --noninteractive flathub com.google.Chrome
+flatpak install --system --noninteractive flathub com.raggesilver.BlackBox
+flatpak install --system --noninteractive flathub com.slack.Slack
+flatpak install --system --noninteractive flathub com.spotify.Client
+flatpak install --system --noninteractive flathub org.gnome.meld
+flatpak install --system --noninteractive flathub org.libreoffice.LibreOffice
+flatpak install --system --noninteractive flathub org.mozilla.firefox
+flatpak install --system --noninteractive flathub org.mozilla.Thunderbird
+flatpak install --system --noninteractive flathub org.telegram.desktop
+flatpak install --system --noninteractive flathub us.zoom.Zoom
 
 echo "Debug installed Flatpaks"
 flatpak list
